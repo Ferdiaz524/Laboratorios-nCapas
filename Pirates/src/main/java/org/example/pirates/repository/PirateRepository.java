637aef2898f4ab61d9b9abe82ffd5e0e62c5815d
Ferdiaz524 <00351519@uca.edu.sv>
2026-05-07 23:51:22 -0600

    :memo:: adding pirate repository

---
+++ Pirates/src/main/java/org/example/pirates/repository/PirateRepository.java
@@ -0,0 +1,11 @@
+package org.example.pirates.repository;
+
+import org.example.pirates.domain.entity.Pirate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PirateRepository extends JpaRepository<Pirate, UUID> {
+}
