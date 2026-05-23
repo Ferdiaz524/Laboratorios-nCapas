a3dd84d98474b2ca841799bef92f6f8cf495d15b
Ferdiaz524 <00351519@uca.edu.sv>
2026-05-23 17:57:12 -0600

    :memo: adding Specimen Controller

---
+++ Labo3-censoHyrule/src/test/java/org/example/labo3censohyrule/Labo3CensoHyruleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.labo3censohyrule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo3CensoHyruleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
